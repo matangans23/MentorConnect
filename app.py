f05bfdc6c6f6fbb097cf52e00607ff00c42d87dd
grusu6928 <56009869+grusu6928@users.noreply.github.com>
2020-01-25 12:48:41 -0500

    Added registration backend support

--- app.py
+++ app.py
@@ -21,6 +21,17 @@ from werkzeug import secure_filename
 from flask import send_file, send_from_directory, safe_join, abort
 from flask import current_app
 
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ['APP_SECRET']
+#app.config['MONGO_URI'] = 'mongodb://localhost:27017/logindb'
+#mongo = PyMongo(app)
+#db = mongo.db
+
+Bootstrap(app)
+
+
 class LoginForm(FlaskForm):
     email_or_user = StringField('Email or username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
@@ -37,3 +48,68 @@ class RegistrationForm(FlaskForm):
     confirm = PasswordField('Repeat Password')
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Register')
+    
+    
+ 
+
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login' # route or function where login occurs...
+
+    
+@app.route('/')
+@login_required
+def index():
+    return redirect(url_for('internships'))
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query(form.email_or_user.data)
+
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            return redirect(url_for('internships'))
+        flash('invalid username or password.')
+
+    return render_template('login.html', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+
+    # force logout
+    logout_user()
+
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        email = form.email.data
+        name = form.username.data
+        user = User(email=email, name=name)
+        user.password = form.password.data # this calls the hash setter
+        try:
+            user.tomongo()
+            login_user(user, form.remember_me.data)
+            return redirect(url_for('internships'))
+        except Exception as e:
+            flash(str(e))
+
+    return render_template('register.html', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out')
+    return redirect(url_for('login'))
+
+
+
+    
+if __name__ == '__main__':
+ app.run(debug=True, port=5000)
