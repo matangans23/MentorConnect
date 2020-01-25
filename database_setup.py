5b93529f6349216cbd2d97ed9fab570826fb4cae
hossam-zaki <hossam_zaki@brown.edu>
2020-01-25 14:26:43 -0500

    added algorithm for getting highest ranked mentors

--- database_setup.py
+++ database_setup.py
@@ -9,11 +9,12 @@ client = MongoClient('localhost', port=27017)
 db = client["mentor_connect"]
 
 collMentors = db["mentors"]
-
+collMentors.drop()
 collMentees = db["mentees"]
-
-def addMentee(name, year, concentration, courses_taken, planned_courses, areas_of_help):
+collMentees.drop()
+def addMentee(brown_id, name, year, concentration, courses_taken, planned_courses, areas_of_help):
     to_load = {
+        "brown_id": brown_id,
         "name" : name,
         "year" : year,
         "concentration" : concentration,
@@ -23,8 +24,9 @@ def addMentee(name, year, concentration, courses_taken, planned_courses, areas_o
     }
     collMentees.insert_one(to_load)
 
-def addMentor(name, year, concentration, courses, areas_of_help):
+def addMentor(brown_id, name, year, concentration, courses, areas_of_help):
     to_load = {
+        "brown_id" : brown_id,
         "name" : name,
         "year" : year,
         "concentration" : concentration,
@@ -33,4 +35,12 @@ def addMentor(name, year, concentration, courses, areas_of_help):
     }
     collMentors.insert_one(to_load)
 
-addMentee("Hossam", "2022", "computational Biology", "CSCI 0150", "CSCI 0160", "[YNYNY]")
+addMentee("hzaki1", "Hossam", "2022", "Computational Biology", ["CSCI 0150", "CSCI 0160", "CSCI1810" ], ["CSCI 1320", "CSCI 1430", "CSCI 1950Y"] , ["Y", "Y", "N", "N", "Y"])
+
+addMentor("nahmed1", "Nasheath", "2019", "Computational Biology", ["CSCI 0150", "CSCI 0160", "CSCI1810", "CSCI 1320", "CSCI 1430", "CSCI 1950Y" ], ["Y", "Y", "N", "N", "Y"])
+
+addMentor("mgans23", "Matan", "2020", "Computer Science", ["CSCI 0150", "CSCI 0160", "CSCI 0060", "CSCI 1951A", "CSCI 1950Y" ], ["Y", "N", "Y", "N", "N"])
+
+addMentor("grusu", "Matan", "2020", "Computational Biology", ["CSCI 0150", "CSCI 0160", "CSCI 0060", "CSCI 1951A", "CSCI 1950Y" ], ["Y", "Y", "N", "N", "Y"])
+
+addMentor("iting", "Matan", "2022", "Computational Biology", ["CSCI 0150", "CSCI 0160", "CSCI 0060", "CSCI 1951A", "CSCI 1410", "CSCI 2020", "CSCI 1310"], ["Y", "Y", "N", "N", "Y"])
